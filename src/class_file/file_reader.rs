d7afaf31e144e6fe3f7bc15052c9ba8ef7f05a6e
Guilherme Lazari <guilhermelcs@gmail.com>
2024-07-04 15:08:33 -0300

    Add a file reader with helper methods

---
+++ src/class_file/file_reader.rs
@@ -0,0 +1,55 @@
+use std::io::Read;
+use std::fs::File;
+use std::io::Result;
+
+pub struct FileReader {
+    file: File,
+}
+
+// The type names that are used in the class file documentation
+type U1 = u8;
+type U2 = [u8; 2];
+type U4 = [u8; 4];
+
+
+impl FileReader {
+    pub fn new(filename: &str) -> Result<FileReader> {
+        let f = FileReader {
+            file: File::open(filename)?
+        };
+        Ok(f)
+    }
+    
+
+    pub fn read_u1(&mut self) -> Result<U1> {
+        let mut buf = [0; 1];
+        self.file.read_exact(&mut buf)?;
+        Ok(buf[0])
+    }
+
+    pub fn read_u2(&mut self) -> Result<U2> {
+        let mut buf = [0; 2];
+        self.file.read_exact(&mut buf)?;
+        Ok(buf)
+    }
+
+    pub fn read_u2_to_u16(&mut self) -> Result<u16> {
+        let mut buf = [0; 2];
+        self.file.read_exact(&mut buf)?;
+        Ok(u16::from_be_bytes(buf))
+    }
+
+    pub fn read_u4(&mut self) -> Result<U4> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        Ok(buf)
+    }
+
+    pub fn read_string(&mut self,) -> Result<String> {
+        let length = self.read_u2_to_u16()?;
+        let mut buf = vec![0; length as usize];
+        self.file.read_exact(&mut buf)?;
+        let s = String::from_utf8(buf.clone()).unwrap();
+        Ok(s)
+    }
+}
--- src/class_file/mod.rs
+++ src/class_file/mod.rs
@@ -26,56 +26,59 @@ ClassFile {
  *
  */
 
-use std::io::Read;
-use std::io::Result;
-use std::fs::File;
+mod file_reader;
 
-type U1 = u8;
-type U2 = [u8; 2];
-type U4 = [u8; 4];
+use file_reader::FileReader;
+use std::io::Result;
 
 pub fn read_class_file(filename: &str) -> Result<()> {
-    let mut file = File::open(filename).unwrap();
 
-    let magic = read_u4(&mut file);
+    let mut file = FileReader::new(filename)?;
+
+    let magic = file.read_u4()?;
     println!("magic: {:?}", magic);
     println!("magic: {:x?}", magic);
     // magic number is 0xCAFEBABE
     assert_eq!(magic, [0xCA, 0xFE, 0xBA, 0xBE]);
 
-    let minor_version = read_u2(&mut file);
-    let major_version = read_u2(&mut file);
+    let minor_version = file.read_u2()?;
+    let major_version = file.read_u2()?;
     println!("versions");
     println!("minor, major: {:?}, {:?}", minor_version, major_version);
 
-
-    let constant_pool_count = read_u2(&mut file);
-    println!("constant_pool_count: {:?}", constant_pool_count);
-    let constant_pool_count = u16::from_be_bytes(constant_pool_count);
+    let constant_pool_count = file.read_u2_to_u16()?;
     println!("constant_pool_count: {}", constant_pool_count);
 
     let mut constant_pool = Vec::new();
-    for i in 1..constant_pool_count {
-        let tag = read_u1(&mut file);
+    for _i in 1..constant_pool_count {
+        let tag = file.read_u1()?;
         println!("tag: {:?}", tag);
 
-
         let constant = match tag {
-            10 => parse_methodref_info(&mut file),
-            7 => parse_class_info(&mut file),
-            12 => parse_name_and_type_info(&mut file),
-            1 => parse_utf8_info(&mut file),    
-            9 => parse_field_ref_info(&mut file),
-            8 => parse_string_info(&mut file),
-            _ => { 
+            METHOD_REF => ConstantPoolInfo::MethodRefInfo(MethodRefInfo {
+                class_index: file.read_u2_to_u16()?,
+                name_and_type_index: file.read_u2_to_u16()?,
+            }),
+            CLASS => ConstantPoolInfo::ClassInfo(ClassInfo {
+                name_index: file.read_u2_to_u16()?,
+            }),
+            NAME_AND_TYPE => ConstantPoolInfo::NameAndTypeInfo(NameAndTypeInfo {
+                name_index: file.read_u2_to_u16()?,
+                descriptor_index: file.read_u2_to_u16()?,
+            }),
+            UTF8 => ConstantPoolInfo::Utf8Info(file.read_string()?),
+            FIELD_REF => ConstantPoolInfo::FieldRefInfo(FieldRefInfo {
+                class_index: file.read_u2_to_u16()?,
+                name_and_type_index: file.read_u2_to_u16()?,
+            }),
+            STRING => parse_string_info(&mut file)?,
+            _ => {
                 println!("tag not implemented: {}", tag);
                 panic!();
             }
         };
 
         constant_pool.push(constant);
-
-
     }
     //println!("constant_pool: {:#?}", constant_pool);
 
@@ -84,47 +87,59 @@ pub fn read_class_file(filename: &str) -> Result<()> {
         println!("{}: {}", i, print_constant_pool_info(info, &constant_pool));
     }
 
-
     Ok(())
 }
 
+const UTF8: u8 = 1;
+const METHOD_REF: u8 = 10;
+const CLASS: u8 = 7;
+const NAME_AND_TYPE: u8 = 12;
+const FIELD_REF: u8 = 9;
+const STRING: u8 = 8;
 
 fn print_constant_pool_info(info: &ConstantPoolInfo, pool: &Vec<ConstantPoolInfo>) -> String {
     match info {
         ConstantPoolInfo::Utf8Info(s) => s.clone(),
         ConstantPoolInfo::NameAndTypeInfo(n) => {
-            let name = get_from_constant_pool(n.name_index, pool); 
+            let name = get_from_constant_pool(n.name_index, pool);
             let descriptor = get_from_constant_pool(n.descriptor_index, pool);
             let name = print_constant_pool_info(name, pool);
             let descriptor = print_constant_pool_info(descriptor, pool);
-            format!("name: {}
-description: {}", name, descriptor)
-        
-        },
+            format!(
+                "name: {}
+description: {}",
+                name, descriptor
+            )
+        }
         ConstantPoolInfo::ClassInfo(c) => {
             let name = get_from_constant_pool(c.name_index, pool);
             let name = print_constant_pool_info(name, pool);
             format!("class {} ", name)
-        },
+        }
         ConstantPoolInfo::MethodRefInfo(m) => {
-            let class = get_from_constant_pool(m.class_index, pool); 
+            let class = get_from_constant_pool(m.class_index, pool);
             let name_and_type = get_from_constant_pool(m.name_and_type_index, pool);
             let class = print_constant_pool_info(class, pool);
-            let name_and_type =  print_constant_pool_info(name_and_type, pool);
-            format!("Method:
+            let name_and_type = print_constant_pool_info(name_and_type, pool);
+            format!(
+                "Method:
 - class: {}
-- name_and_type: {}", class, name_and_type)
-        },
+- name_and_type: {}",
+                class, name_and_type
+            )
+        }
         ConstantPoolInfo::FieldRefInfo(f) => {
-            let class = get_from_constant_pool(f.class_index, pool); 
+            let class = get_from_constant_pool(f.class_index, pool);
             let name_and_type = get_from_constant_pool(f.name_and_type_index, pool);
             let class = print_constant_pool_info(class, pool);
-            let name_and_type =  print_constant_pool_info(name_and_type, pool);
-            format!("Field:
+            let name_and_type = print_constant_pool_info(name_and_type, pool);
+            format!(
+                "Field:
 - class: {}
-- name_and_type {}", class, name_and_type)
-        
-        },
+- name_and_type {}",
+                class, name_and_type
+            )
+        }
         ConstantPoolInfo::StringInfo(s) => {
             let string = get_from_constant_pool(s.string_index, pool);
             let string = print_constant_pool_info(string, pool);
@@ -136,8 +151,6 @@ description: {}", name, descriptor)
 fn get_from_constant_pool(index: u16, pool: &Vec<ConstantPoolInfo>) -> &ConstantPoolInfo {
     &pool[index as usize - 1]
 }
-
-
 
 #[derive(Debug)]
 enum ConstantPoolInfo {
@@ -177,65 +190,7 @@ struct StringInfo {
     string_index: u16,
 }
 
-
-fn parse_string_info(file: &mut File) -> ConstantPoolInfo {
-    let string_index = read_u2_to_u16(file);
-    ConstantPoolInfo::StringInfo(StringInfo { string_index })
-}
-
-fn parse_field_ref_info(file: &mut File) -> ConstantPoolInfo {
-    let class_index = read_u2_to_u16(file); 
-    let name_and_type_index = read_u2_to_u16(file); 
-    ConstantPoolInfo::FieldRefInfo(FieldRefInfo { class_index, name_and_type_index })
-}
-
-
-fn parse_utf8_info(file: &mut File) -> ConstantPoolInfo {
-    let length = read_u2_to_u16(file); 
-    let mut buf = vec![0; length as usize];
-    file.read_exact(&mut buf).unwrap();
-    let s = String::from_utf8(buf.clone()).unwrap();
-    ConstantPoolInfo::Utf8Info(s)
-}
-
-fn parse_name_and_type_info(file: &mut File) -> ConstantPoolInfo {
-    let name_index = read_u2_to_u16(file);
-    let descriptor_index = read_u2_to_u16(file);
-    ConstantPoolInfo::NameAndTypeInfo(NameAndTypeInfo { name_index, descriptor_index })
-}
-
-fn parse_class_info(file: &mut File) -> ConstantPoolInfo {
-    let name_index = read_u2_to_u16(file);
-    ConstantPoolInfo::ClassInfo(ClassInfo { name_index })
-}
-
-fn parse_methodref_info(file: &mut File) -> ConstantPoolInfo {
-    let class_index = read_u2_to_u16(file);
-    let name_and_type_index = read_u2_to_u16(file);
-    ConstantPoolInfo::MethodRefInfo(MethodRefInfo { class_index, name_and_type_index })
-}
-
-
-fn read_u1(file: &mut File) -> U1 {
-    let mut buf = [0; 1];
-    file.read_exact(&mut buf).unwrap();
-    buf[0]
-}
-
-fn read_u2(file: &mut File) -> U2 {
-    let mut buf = [0; 2];
-    file.read_exact(&mut buf).unwrap();
-    buf
-}
-
-fn read_u2_to_u16(file: &mut File) -> u16 {
-    let mut buf = [0; 2];
-    file.read_exact(&mut buf).unwrap();
-    u16::from_be_bytes(buf)
-}
-
-fn read_u4(file: &mut File) -> U4 {
-    let mut buf = [0; 4];
-    file.read_exact(&mut buf).unwrap();
-    buf
+fn parse_string_info(file: &mut FileReader) -> Result<ConstantPoolInfo> {
+    let string_index = file.read_u2_to_u16()?;
+    Ok(ConstantPoolInfo::StringInfo(StringInfo { string_index }))
 }
