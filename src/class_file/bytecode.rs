9e29522421b06f6343a1e446d226bd38e5f5d863
Guilherme Lazari <guilhermelcs@gmail.com>
2024-07-15 06:16:40 -0300

    Finish adding bytecodes needed for example

--- src/class_file/bytecode.rs
+++ src/class_file/bytecode.rs
@@ -10,8 +10,12 @@ pub enum ByteCode {
     Duplicate,
     AReturn,
     ALoad(u8),
+    Lload(u8),
+    PutStatic(u16),
     Ldc(u8),
     Return,
+    LReturn,
+    LAdd,
     Generic(u8),
 }
 
@@ -26,8 +30,12 @@ impl ByteCode {
             0xb7 => (ByteCode::InvokeSpecial(file.read_u2_to_u16()?), 3),
             0xb0 => (ByteCode::AReturn, 1),
             0xb1 => (ByteCode::Return, 1),
+            0xad => (ByteCode::LReturn, 1),
             0x12 => (ByteCode::Ldc(file.read_u1()?), 2),
+            0xb3 => (ByteCode::PutStatic(file.read_u2_to_u16()?), 3),
             0x2a..=0x2d => (ByteCode::ALoad(opcode - 0x2a), 1),
+            0x1e..=0x21 => (ByteCode::Lload(opcode - 0x1e), 1),
+            0x61 => (ByteCode::LAdd, 1),
             _ => (ByteCode::Generic(opcode), 1),
         };
         Ok((code, len))
@@ -42,8 +50,12 @@ impl ByteCode {
             ByteCode::InvokeSpecial(u16) => format!("InvokeSpecial(0x{:x?})", u16),
             ByteCode::AReturn => "Reference Return (areturn)".to_string(),
             ByteCode::Return => "Return void (return)".to_string(),
+            ByteCode::LReturn => "Long Return (lreturn)".to_string(),
             ByteCode::Ldc(u8) => format!("Ldc({})", u8),
             ByteCode::ALoad(u8) => format!("ALoad({})", u8),
+            ByteCode::Lload(u8) => format!("Lload({})", u8),
+            ByteCode::PutStatic(u16) => format!("PutStatic(0x{:x?})", u16),
+            ByteCode::LAdd => "LAdd".to_string(),
             ByteCode::Generic(u8) => format!("Generic(0x{:x?})", u8),
         }
     }
